00cb24c187d12902df258937be6f291602d49ec3
Alexander Sakhaev <alsakhaev@gmail.com>
2023-10-11 13:23:23 +0200

    refactor: remove temp comments

--- dapplet/src/index.ts
+++ dapplet/src/index.ts
@@ -1,59 +1,6 @@
 import {} from "@dapplets/dapplet-extension";
 import users from "./near-social-users.json";
 
-/*
-mybadge.near/widget/BadgeDapplet.Profile.Avatar.Near
-
-mybadge.near/widget/BadgeDapplet.Profile.Fullname.Dapplets
-mybadge.near/widget/BadgeDapplet.Profile.Fullname.Lnc
-mybadge.near/widget/BadgeDapplet.Profile.Fullname.DevHacks
-
-mybadge.near/widget/BadgeDapplet.Post.Fullname.Dapplets
-mybadge.near/widget/BadgeDapplet.Post.Fullname.Lnc
-mybadge.near/widget/BadgeDapplet.Post.Fullname.DevHacks
-
-mybadge.near/widget/BadgeDapplet.Post.BeforeText.LncElite
-mybadge.near/widget/BadgeDapplet.Post.BeforeText.NearVeteran
-mybadge.near/widget/BadgeDapplet.Post.BeforeText.DevHacks
-
-mybadge.near/widget/BadgeDapplet.ProfilePopup.AfterUsername.ProTester
-mybadge.near/widget/BadgeDapplet.ProfilePopup.AfterUsername.ProTester
-mybadge.near/widget/BadgeDapplet.ProfilePopup.AfterUsername.DappCreator
-mybadge.near/widget/BadgeDapplet.ProfilePopup.AfterUsername.AlphaTester
-
-// GENERIC COMPONENTS
-
-mybadge.near/widget/Generic.AvatarBadge
-  icon
-
-mybadge.near/widget/Generic.CircleBadge
-  backgroundColor
-  icon
-
-mybadge.near/widget/Generic.RectBadge
-  iconBackgroundColor
-  textBackgroundColor
-  icon
-  textColor
-
-mybadge.near/widget/Generic.DetailedRectBadge
-  backgroundColor
-  textColor
-  primaryText
-  secondaryText
-
-// NEAR
-
-mybadge.near/widget/Near.AvatarBadge
-
-// Dapplets
-
-mybadge.near/widget/Dapplets.AvatarBadge
-mybadge.near/widget/Dapplets.CircleBadge
-mybadge.near/widget/Dapplets.DappCreator
-
-*/
-
 @Injectable
 export default class {
   @Inject("twitter-bos-config")
