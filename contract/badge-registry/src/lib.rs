4806205b3af23862c065497c72b4415861791643
Alexander Sakhaev <alsakhaev@gmail.com>
2023-10-12 17:59:11 +0200

    feat: introduce badge-registry

--- contract/badge-registry/src/lib.rs
+++ contract/badge-registry/src/lib.rs
@@ -1,64 +1,170 @@
-// Find all our documentation at https://docs.near.org
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::{env, near_bindgen, AccountId};
 
-// Define the default message
-const DEFAULT_MESSAGE: &str = "Hello";
+type BadgeType = String;
+type BadgeId = String;
 
-// Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    message: String,
+pub struct BadgeRegistry {
+    owner: AccountId,
+    badge_types: UnorderedMap<BadgeId, BadgeType>,
+    user_badges: UnorderedMap<AccountId, Vec<BadgeId>>,
 }
 
-// Define the default, which automatically initializes the contract
-impl Default for Contract{
-    fn default() -> Self{
-        Self{message: DEFAULT_MESSAGE.to_string()}
+impl Default for BadgeRegistry {
+    fn default() -> Self {
+        Self {
+            owner: "mybadge.near".parse().unwrap(),
+            badge_types: UnorderedMap::new(b"badge_types".to_vec()),
+            user_badges: UnorderedMap::new(b"user_badges".to_vec()),
+        }
     }
 }
 
-// Implement the contract structure
 #[near_bindgen]
-impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    pub fn get_greeting(&self) -> String {
-        return self.message.clone();
+impl BadgeRegistry {
+    #[init]
+    #[private]
+    pub fn init() -> Self {
+        Self {
+            owner: env::predecessor_account_id(),
+            badge_types: UnorderedMap::new(b"badge_types".to_vec()),
+            user_badges: UnorderedMap::new(b"user_badges".to_vec()),
+        }
     }
 
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, message: String) {
-        log!("Saving greeting {}", message);
-        self.message = message;
+    pub fn get_badges_by_account(
+        &self,
+        account_id: AccountId,
+    ) -> Vec<(BadgeId, BadgeType)> {
+        if let Some(badges) = self.user_badges.get(&account_id) {
+            let badges_info: Vec<(BadgeId, BadgeType)> = badges
+                .iter()
+                .filter_map(|badge_id| {
+                    if let Some(badge_type) = self.badge_types.get(&badge_id) {
+                        Some((badge_id.clone(), badge_type.clone()))
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+            badges_info
+        } else {
+            vec![]
+        }
+    }
+
+    pub fn get_all_badges(&self) -> Vec<(BadgeId, BadgeType)> {
+        let mut all_badges = vec![];
+        for (badge_id, badge_type) in self.badge_types.iter() {
+            all_badges.push((badge_id, badge_type));
+        }
+        all_badges
+    }
+
+    pub fn add_badge(&mut self, badge_id: BadgeId, badge_type: BadgeType) {
+        self.only_owner();
+        self.badge_types.insert(&badge_id, &badge_type);
+    }
+
+    pub fn add_badge_to_account(&mut self, badge_id: BadgeId, account_id: AccountId) {
+        self.only_owner();
+        if let Some(_) = self.badge_types.get(&badge_id) {
+            let mut user_badges = self.user_badges.get(&account_id).unwrap_or_else(|| vec![]);
+            user_badges.push(badge_id.clone());
+            self.user_badges.insert(&account_id, &user_badges);
+        } else {
+            env::panic_str("Badges: no badge found");
+        }
+    }
+
+    pub fn remove_badge_from_account(&mut self, badge_id: BadgeId, account_id: AccountId) {
+        self.only_owner();
+        if let Some(mut user_badges) = self.user_badges.get(&account_id) {
+            if let Some(position) = user_badges.iter().position(|id| id == &badge_id) {
+                user_badges.remove(position);
+                self.user_badges.insert(&account_id, &user_badges);
+            }
+        }
+    }
+
+    pub fn owner(&self) -> AccountId {
+        self.owner.clone()
+    }
+
+    pub fn transfer_ownership(&mut self, new_owner: AccountId) {
+        self.only_owner();
+        self.owner = new_owner;
+    }
+
+    fn only_owner(&self) {
+        assert_eq!(
+            &env::predecessor_account_id(),
+            &self.owner,
+            "Only the owner can call this method."
+        );
     }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
 #[cfg(test)]
 mod tests {
     use super::*;
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::testing_env;
+
+    const ACCOUNT_ID_1: &str = "account1.testnet";
 
     #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
+    fn test_add_badge() {
+        let mut contract = BadgeRegistry::init();
+        contract.add_badge("badge1".to_string(), "type1".to_string());
+        let badges = contract.get_all_badges();
+        assert_eq!(badges.len(), 1);
     }
 
     #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
+    fn test_add_badge_to_account() {
+        let mut contract = BadgeRegistry::init();
+        contract.add_badge("badge1".to_string(), "type1".to_string());
+        contract.add_badge_to_account("badge1".to_string(), ACCOUNT_ID_1.parse().unwrap());
+        let badges = contract.get_badges_by_account(ACCOUNT_ID_1.parse().unwrap());
+        assert_eq!(badges.len(), 1);
+    }
+
+    #[test]
+    fn test_remove_badge_from_account() {
+        let mut contract = BadgeRegistry::init();
+        contract.add_badge("badge1".to_string(), "type1".to_string());
+        contract.add_badge_to_account("badge1".to_string(), ACCOUNT_ID_1.parse().unwrap());
+        contract.remove_badge_from_account("badge1".to_string(), ACCOUNT_ID_1.parse().unwrap());
+        let badges = contract.get_badges_by_account(ACCOUNT_ID_1.parse().unwrap());
+        assert_eq!(badges.len(), 0);
+    }
+
+    #[test]
+    fn test_transfer_ownership() {
+        let mut contract = BadgeRegistry::init();
+        let new_owner = AccountId::new_unchecked("new_owner.near".to_string());
+        contract.transfer_ownership(new_owner.clone());
+
+        let owner = contract.owner();
+        assert_eq!(owner, new_owner.clone());
+    }
+
+    #[test]
+    #[should_panic(expected = "Only the owner can call this method.")]
+    fn test_non_owner_transfer_ownership() {
+        let mut contract = BadgeRegistry::init();
+        let new_owner = AccountId::new_unchecked("new_owner.near".to_string());
+        set_context("third.near");
+        contract.transfer_ownership(new_owner.clone());
+    }
+
+    fn set_context(predecessor: &str) {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor.parse().unwrap());
+        testing_env!(builder.build());
     }
 }
